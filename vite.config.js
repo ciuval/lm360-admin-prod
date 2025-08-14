aca67d5c9165d4b0a2d665ad2c0be4fae0b057e8
Valeriu Ciubotaru <la-tua-email@dominio.tld>
2025-08-14 14:58:44 +0200

    fix: remove merge markers from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
+import { Analytics } from "@vercel/analytics/next"export default 
+defineConfig({
   plugins: [react()],
   build: { chunkSizeWarningLimit: 1500 }
 })
+
