ec2bf83f74836d8c2bd3affccde839ced04df8d7
Ellav <ciuval@users.noreply.github.com>
2026-05-31 13:11:46 +0200

    Load admin dashboard directly

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,23 +1,3 @@
-import React from "react";
+import AdminPage from "./AdminPage.jsx";
 
-const mods = import.meta.glob(
-  ["./AdminPage.jsx","./AdminPanel.jsx","./DashboardAdmin.jsx"],
-  { eager: true }
-);
-
-const mod =
-  mods["./AdminPage.jsx"] ||
-  mods["./AdminPanel.jsx"] ||
-  mods["./DashboardAdmin.jsx"] ||
-  Object.values(mods)[0];
-
-const Admin = (mod && mod.default) || function AdminFallback() {
-  return (
-    <section style={{ padding: 24 }}>
-      <h1>Admin</h1>
-      <p>Pagina admin non disponibile in questa build.</p>
-    </section>
-  );
-};
-
-export default Admin;
+export default AdminPage;
